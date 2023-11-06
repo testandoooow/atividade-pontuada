ca8b5c8aad7c9f7354691933c9abbb06d6c76f03
Kaique.s <141377576+testandoooow@users.noreply.github.com>
2023-11-06 11:29:57 -0300

    Update 8 questao.c

--- 8 questao.c
+++ 8 questao.c
@@ -8,43 +8,23 @@ int main(){
     
     for(i = numero; i >= 0; i--){
         printf("\n%d",i);
-        switch(i){
-            case 0:
-            printf(" End");
-            break;
-            case 1:
-            printf(" Versao brasileira hebeterriches\n");
-            break;
-            case 2:
-            printf(" ooooo\n");
-            break;
-            case 3:
-            printf(" epa!\n");
-            break;
-            case 4:
-            printf(" CAVALO!\n");
-            break;
-            case 5:
-            printf(" hehe\n");
-            break;
-            case 6:
-            printf(" ez\n");
-            break;
-            case 7:
-            printf(" haha\n");
-            break;
-            case 8:
-            printf(" Vasco\n");
-            break;
-            case 9:
-            printf(" Tamarindo\n");
-            break;
-            case 10:
-            printf(" Cebola\n");
-            break;
-            default:
-            printf(" E\n");
-        }
-    }
+        if(i <= 4){
+        	printf(" hehe");
+		} else if(i <= 8){
+			printf(" ez");
+		} else if(i <= 15){
+			printf(" abacate");
+		} else if(i <= 26){
+			printf(" ABACAXI");
+		} else if(i <= 40){
+			printf(" ooooo");
+		} else if(i <= 60){
+			printf(" Cebola");
+		} else if(i <= 90){
+			printf("Tamarindo");
+		} else if(i > 90){
+			printf("Versao brasileira hebeterriches");
+		}
+	}
     return 0;
 }
